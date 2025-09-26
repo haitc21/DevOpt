cb55bd852f2b969a113d0ca965d7d1a0bb3ce55c
trancaohai@vnpt.vn <trancaohai>
2025-09-26 15:25:57 +0700

    gitlab runner

--- DevSecOps/ecommerce-fullstack-netcore-react/backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
+++ DevSecOps/ecommerce-fullstack-netcore-react/backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b0a6aa034e90b8ec386094a48221bcb118e31c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394741a969c7be9d436469767a3110c3e0dccc15")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
